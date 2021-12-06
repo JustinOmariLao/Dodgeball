222731e6f861e0ce5beedf8c82fc225d2ccd9059
ejrossma <ejrossma@ucsc.edu>
2021-12-05 22:13:00 -0800

    Menu Scene Changes

--- Dodgeball/Assets/Scripts/MenuScript.cs
+++ Dodgeball/Assets/Scripts/MenuScript.cs
@@ -5,20 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void playGame() {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("PlayScene");
     }
 
     public void quitGame() {
